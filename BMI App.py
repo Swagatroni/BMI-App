2e3bac8202a164c9f3b787555583ef737b312d85
Kenn <kennethwthomasjr@hotmail.com>
2024-03-04 13:43:14 -0600

    added unit conversions to bmi_calculations function

--- BMI App.py
+++ BMI App.py
@@ -2,21 +2,12 @@
 # kwt61
 # BMI App
 
-# Body Mass Index:
-    # Height - feet and inches
-    # Weight - pounds
 
-# Calculate BMI value and category:
-    # < 18.5 - Underweight 
-    # 18.5–24.9 - Normal weight 
-    # 25–29.9 - Overweight 
-    # >=30 - Obese
+def calculate_bmi(weight, height_ft, height_in):
+    weight_kg = weight * 0.45
+    height_m = (height_ft * 12 + height_in) * 0.025
 
-def calculate_bmi(weight, height):
-    weight = weight * 0.45
-    height = height * 0.025
-    
-    bmi = weight / (height ** 2)
+    bmi = weight_kg / (height_m ** 2)
     return bmi
 
 def classify(val):
@@ -36,9 +27,8 @@ w = float(input("Enter your weight in pounds: "))
 
 h_ft = int(input("Enter your height in feet: "))
 h_in = int(input("Enter your height in inches: "))
-h = h_ft * 12 + h_in
 
-bmi_val = calculate_bmi(w, h)
+bmi_val = calculate_bmi(w, h_ft, h_in)
 print("Your BMI is:", bmi_val)
 classify(bmi_val)
 
